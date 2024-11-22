cfa09314e80aee038ec041f43269c4a83c2a90f6
ALI ZULFIQAR <alimalikali1928@gmail.com>
2024-11-23 01:42:48 +0500

    Phase 1 : (A.P.I)Backend : Courses api created (get all courses and get 1 course) and Database DynamoDB configured

---
+++ server/src/controllers/courseController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import Course from "../models/courseModel";
+import AWS from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
+
+const s3 = new AWS.S3();
+
+export const listCourses = async ( req: Request, res: Response): Promise<void> => {
+  const { category } = req.query;
+  try {
+    const courses =
+      category && category !== "all"
+        ? await Course.scan("category").eq(category).exec()
+        : await Course.scan().exec();
+    res.json({ message: "Courses retrieved successfully", data: courses });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving courses", error });
+  }
+};
+
+export const getCourse = async (req: Request, res: Response): Promise<void> => {
+  const { courseId } = req.params;
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: "Course not found" });
+      return;
+    }
+
+    res.json({ message: "Course retrieved successfully", data: course });
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving course", error });
+  }
+};
+
+export const createCourse = async ( req: Request, res: Response): Promise<void> => {
+  try {
+    const { teacherId, teacherName } = req.body;
+
+    if (!teacherId || !teacherName) {
+      res.status(400).json({ message: "Teacher Id and name are required" });
+      return;
+    }
+
+    const newCourse = new Course({
+      courseId: uuidv4(),
+      teacherId,
+      teacherName,
+      title: "Untitled Course",
+      description: "",
+      category: "Uncategorized",
+      image: "",
+      price: 0,
+      level: "Beginner",
+      status: "Draft",
+      sections: [],
+      enrollments: [],
+    });
+    await newCourse.save();
+
+    res.json({ message: "Course created successfully", data: newCourse });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating course", error });
+  }
+};
+
