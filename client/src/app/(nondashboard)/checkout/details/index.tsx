5b4a5a0640c363c88f48c7f870bbb9610fe3b2b0
ALI ZULFIQAR <alimalikali1928@gmail.com>
2024-11-23 01:38:12 +0500

    Phase 1 : UI(Front-End) : Single Course page UI and Landing Page fetching data from backend

---
+++ client/src/app/(nondashboard)/checkout/details/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+
+const CheckoutDetailsPage = () => {
+
+  return (
+    <div className="checkout-details">
+      <div className="checkout-details__container">
+
+      </div>
+
+    </div>
+  );
+};
+
+export default CheckoutDetailsPage;
---
+++ client/src/app/(nondashboard)/checkout/payment/index.tsx
@@ -0,0 +1,14 @@
+
+const PaymentPageContent = () => {
+
+  return (
+    <div className="payment">
+    </div>
+  );
+};
+
+const PaymentPage = () => (
+    <PaymentPageContent />
+);
+
+export default PaymentPage;
